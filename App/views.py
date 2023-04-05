7e0ccd512c69ed23df493fd0f8f8610b42028165
Udayanaik95 <naik.udaya25@gmail.com>
2023-04-05 17:48:23 +0530

    first commit

---
+++ App/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from App.models import *
+# Create your views here.
+
+def display_topic(request):
+    LOT=Topic.objects.all()
+
+    d={'topics':LOT}
+
+    return render(request,'display_topic.html',context=d)
+
+def display_webpage(request):
+    LOW=Webpage.objects.all()
+    
+    d={'webpages':LOW}
+
+    return render(request,'display_webpage.html',context=d)
+
